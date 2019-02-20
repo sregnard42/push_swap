9cd802245681ee8ed6027476e19f5c0355de7277
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-20 03:10:18 +0100

    includes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,32 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   common.h                                           :+:      :+:    :+:   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/19 21:46:27 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/20 01:21:03 by sregnard         ###   ########.fr       */
+/*   Created: 2019/02/20 02:20:57 by sregnard          #+#    #+#             */
+/*   Updated: 2019/02/20 02:21:13 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef COMMON_H
-# define COMMON_H
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-# include "libft.h"
-
-# define ERR_DEFAULT "Error\n"
-
-typedef struct	s_ps
-{
-		int		*a;
-		int		*b;
-		int		size_a;
-		int		size_b;
-}				t_ps;
-
-void			trigger_error(char *error);
-void			parse_args(t_ps *p, int ac, char **av);
-void			print_stacks(t_ps p);
+# include "shared.h"
 
 #endif
--- includes/shared.h
+++ includes/shared.h
@@ -1,37 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   checker.h                                          :+:      :+:    :+:   */
+/*   shared.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/18 17:27:57 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/20 03:04:42 by sregnard         ###   ########.fr       */
+/*   Created: 2019/02/19 21:46:27 by sregnard          #+#    #+#             */
+/*   Updated: 2019/02/20 02:37:51 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CHECKER_H
-# define CHECKER_H
+#ifndef SHARED_H
+# define SHARED_H
 
-# include "shared.h"
+# include "libft.h"
 
-# define NO_SLEEP		0
-# define SHORT_SLEEP	1
-# define LONG_SLEEP		2
+# define ERR_DEFAULT "Error\n"
 
-int		swap_a(t_ps *p);
-int		swap_b(t_ps *p);
-int		swap(t_ps *p);
+typedef struct	s_ps
+{
+		int		*a;
+		int		*b;
+		int		size_a;
+		int		size_b;
+		int		flags;
+}				t_ps;
 
-int		push_a(t_ps *p);
-int		push_b(t_ps *p);
+enum			e_flags
+{
+		FLAG_DISPLAY = (1 << 0),
+		FLAG_COLOR = (1 << 1),
+};
 
-int		rotate_a(t_ps *p);
-int		rotate_b(t_ps *p);
-int		rotate(t_ps *p);
-
-int		rev_rotate_a(t_ps *p);
-int		rev_rotate_b(t_ps *p);
-int		rev_rotate(t_ps *p);
+void			trigger_error(char *error);
+void			parse_args(t_ps *p, int ac, char **av);
+void			print_stacks(t_ps p);
 
 #endif
