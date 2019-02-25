d45b98c3467ccd144b9d599ca88e1477303b8a28
Stanley Regnard <sregnard@stanleys-air.home>
2019-02-25 10:19:57 +0100

    includes

--- includes/checker.h
+++ includes/checker.h
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 17:27:57 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/20 03:04:42 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/20 17:29:14 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,5 @@
 # define NO_SLEEP		0
 # define SHORT_SLEEP	1
 # define LONG_SLEEP		2
-
-int		swap_a(t_ps *p);
-int		swap_b(t_ps *p);
-int		swap(t_ps *p);
-
-int		push_a(t_ps *p);
-int		push_b(t_ps *p);
-
-int		rotate_a(t_ps *p);
-int		rotate_b(t_ps *p);
-int		rotate(t_ps *p);
-
-int		rev_rotate_a(t_ps *p);
-int		rev_rotate_b(t_ps *p);
-int		rev_rotate(t_ps *p);
 
 #endif
--- includes/shared.h
+++ includes/shared.h
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 21:46:27 by sregnard          #+#    #+#             */
-/*   Updated: 2019/02/20 11:25:54 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/02/20 17:40:08 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,10 +31,27 @@ enum			e_flags
 {
 		FLAG_DISPLAY = (1 << 0),
 		FLAG_COLOR = (1 << 1),
+		FLAG_SOLVER = (1 << 2),
 };
 
 void			trigger_error(char *error);
 void			parse_args(t_ps *p, int ac, char **av);
 void			print_stacks(t_ps p);
+int				sorted(t_ps p);
+
+int				swap_a(t_ps *p);
+int				swap_b(t_ps *p);
+int				swap(t_ps *p);
+
+int				push_a(t_ps *p);
+int				push_b(t_ps *p);
+
+int				rotate_a(t_ps *p);
+int				rotate_b(t_ps *p);
+int				rotate(t_ps *p);
+
+int				rev_rotate_a(t_ps *p);
+int				rev_rotate_b(t_ps *p);
+int				rev_rotate(t_ps *p);
 
 #endif
