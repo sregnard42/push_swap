3586ecd2057b8c5f7cc1cc515c119df70204bdfb
Stanley Regnard <sregnard@stanleys-air.home>
2019-03-05 13:12:43 +0100

    test

--- my_test.sh
+++ my_test.sh
@@ -6,8 +6,9 @@
 #    By: sregnard <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/20 02:23:04 by sregnard          #+#    #+#              #
-#    Updated: 2019/02/25 16:35:08 by sregnard         ###   ########.fr        #
+#    Updated: 2019/03/04 17:04:02 by sregnard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-ARG="5 4 1 3 2 9 6 7 -c -v"; ./push_swap $ARG | ./checker $ARG
+#ARG="5 4 1 3 2 9 6 7 -c -v"; ./push_swap $ARG | ./checker $ARG
+ARG="5 4 1 3 2 9 6 7 -c -v"; ./push_swap $ARG
