5c3774ac01208d836e90eac43c9414f070d0187e
Stanley Regnard <sregnard@stanleys-air.home>
2019-03-16 19:03:46 +0100

    test

--- my_test.sh
+++ my_test.sh
@@ -6,14 +6,14 @@
 #    By: sregnard <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/20 02:23:04 by sregnard          #+#    #+#              #
-#    Updated: 2019/03/16 14:28:25 by sregnard         ###   ########.fr        #
+#    Updated: 2019/03/16 18:27:05 by sregnard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-ARG=`ruby -e "puts((1..42).to_a.shuffle.join(' '))"`
+ARG=`ruby -e "puts((1..10).to_a.shuffle.join(' '))"`
 
 ./push_swap $ARG | ./checker $ARG -c -v
 ./push_swap $ARG | wc -l
 #./push_swap $ARG
 #./checker $ARG
-#echo $ARG
+echo $ARG
