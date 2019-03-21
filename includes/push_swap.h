8e74f9ef7b60b3118793b5e2cc658786b63a88ac
Stanley Regnard <sregnard@stanleys-air.home>
2019-03-21 16:29:02 +0100

    includes

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/19 21:46:27 by sregnard          #+#    #+#             */
-/*   Updated: 2019/03/20 13:20:41 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/03/21 15:26:50 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,23 +24,13 @@
 
 # define LINES_LIMIT	42
 
-typedef struct	s_op
-{
-		char	*operations;
-		char	*last_op;
-}				t_op;
-
 typedef struct	s_ps
 {
-		t_op	op;
+		char	*operations;
 		int		*a;
 		int		*b;
 		int		size_a;
 		int		size_b;
-		int		flags_a;
-		int		flags_b;
-		int		flags_ab;
-		int		flags_push;
 		int		flags;
 }				t_ps;
 
@@ -49,33 +39,6 @@ enum			e_flags
 				FLAG_DISPLAY = (1 << 0),
 				FLAG_COLOR = (1 << 1),
 				FLAG_SOLVER = (1 << 2),
-};
-
-enum			e_flags_push
-{
-				FLAG_PA = (1 << 0),
-				FLAG_PB = (1 << 1),
-};
-
-enum			e_flags_a
-{
-				FLAG_SA = (1 << 0),
-				FLAG_RA = (1 << 1),
-				FLAG_RRA = (1 << 2),
-};
-
-enum			e_flags_b
-{
-				FLAG_SB = (1 << 0),
-				FLAG_RB = (1 << 1),
-				FLAG_RRB = (1 << 2),
-};
-
-enum			e_flags_ab
-{
-				FLAG_SS = (1 << 1),
-				FLAG_RR = (1 << 2),
-				FLAG_RRR = (1 << 3),
 };
 
 /*
@@ -98,13 +61,15 @@ void			print_stacks(t_ps p, char *s, float interval);
 int				find_pos(t_ps *p, int val, char c);
 int				get_val(t_ps *p, int pos, char c);
 int				goto_pos(t_ps *p, int pos, char c);
+int				(*rotate_function(t_ps *p, int pos, char c))(t_ps *p, char);
 int				sorted(t_ps p, char c, int top, int bottom);
 
 /*
 **				operations.c
 */
 
-int				add_operation(t_op *op, char *new_op);
+int				add_operation(char **operations, char *new_op);
+char			**opti_operations(char **operations);
 
 /*
 **				Allowed operations
