37b0c4f9ea15a258f647bf6a9a6290a0624eb18e
Stanley REGNARD <sregnard@e3r7p18.42.fr>
2019-04-07 10:43:12 +0200

    norm

--- srcs/checker/checker.c
+++ srcs/checker/checker.c
@@ -14,53 +14,53 @@
 
 static int	operation(t_ps *p, char *s)
 {
-		int		(*f)(t_ps *, char c);
-		char	c;
+	int		(*f)(t_ps *, char c);
+	char	c;
 
-		c = s[ft_strlen(s) - 1];
-		f = NULL;
-		ft_strequ(s, "pa") ? f = &push : 0;
-		ft_strequ(s, "pb") ? f = &push : 0;
-		ft_strequ(s, "sa") ? f = &swap : 0;
-		ft_strequ(s, "sb") ? f = &swap : 0;
-		ft_strequ(s, "ss") ? f = &swap : 0;
-		ft_strequ(s, "ra") ? f = &rotate : 0;
-		ft_strequ(s, "rb") ? f = &rotate : 0;
-		ft_strequ(s, "rr") ? f = &rotate : 0;
-		ft_strequ(s, "rra") ? f = &rev_rotate : 0;
-		ft_strequ(s, "rrb") ? f = &rev_rotate : 0;
-		ft_strequ(s, "rrr") ? f = &rev_rotate : 0;
-		if (f)
-				return (f(p, c));
-		return (0);
+	c = s[ft_strlen(s) - 1];
+	f = NULL;
+	ft_strequ(s, "pa") ? f = &push : 0;
+	ft_strequ(s, "pb") ? f = &push : 0;
+	ft_strequ(s, "sa") ? f = &swap : 0;
+	ft_strequ(s, "sb") ? f = &swap : 0;
+	ft_strequ(s, "ss") ? f = &swap : 0;
+	ft_strequ(s, "ra") ? f = &rotate : 0;
+	ft_strequ(s, "rb") ? f = &rotate : 0;
+	ft_strequ(s, "rr") ? f = &rotate : 0;
+	ft_strequ(s, "rra") ? f = &rev_rotate : 0;
+	ft_strequ(s, "rrb") ? f = &rev_rotate : 0;
+	ft_strequ(s, "rrr") ? f = &rev_rotate : 0;
+	if (f)
+		return (f(p, c));
+	return (0);
 }
 
 static void	print_result(t_ps *p, int ok)
 {
-		if (p->flags & FLAG_COLOR)
-				ok ? ft_putendl("\033[32mOK\033[0m")
-						: ft_putendl("\033[31mKO\033[0m");
-		else
-				ok ? ft_putendl("OK") : ft_putendl("KO");
-		ft_memdel((void **)&p->a);
-		ft_memdel((void **)&p->b);
+	if (p->flags & FLAG_COLOR)
+		ok ? ft_putendl("\033[32mOK\033[0m")
+		: ft_putendl("\033[31mKO\033[0m");
+	else
+		ok ? ft_putendl("OK") : ft_putendl("KO");
+	ft_memdel((void **)&p->a);
+	ft_memdel((void **)&p->b);
 }
 
 int			main(int ac, char **av)
 {
-		t_ps	p;
-		char	*line;
-		int		ok;
+	t_ps	p;
+	char	*line;
+	int		ok;
 
-		parse_args(&p, ac, av);
-		if (p.flags & FLAG_DISPLAY)
-				print_stacks(p, "Init A and B", SLP_SHORT);
-		while (!(ok = sorted(p, 0, 0, 0)) && get_next_line(0, &line))
-		{
-				!(operation(&p, line)) ? trigger_error(line) : 0;
-				p.flags & FLAG_DISPLAY ? print_stacks(p, line, SLP_NONE) : 0;
-				ft_memdel((void **)&line);
-		}
-		print_result(&p, ok);
-		exit(EXIT_SUCCESS);
+	parse_args(&p, ac, av);
+	if (p.flags & FLAG_DISPLAY)
+		print_stacks(p, "Init A and B", SLP_SHORT);
+	while (!(ok = sorted(p, 0, 0, 0)) && get_next_line(0, &line))
+	{
+		!(operation(&p, line)) ? trigger_error(line) : 0;
+		p.flags & FLAG_DISPLAY ? print_stacks(p, line, SLP_NONE) : 0;
+		ft_memdel((void **)&line);
+	}
+	print_result(&p, ok);
+	exit(EXIT_SUCCESS);
 }
