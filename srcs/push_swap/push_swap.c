f0f7589ea720ec5621449ec75291de6cec6cc436
Stanley REGNARD <sregnard@e3r7p18.42.fr>
2019-04-07 10:42:54 +0200

    norm

--- srcs/push_swap/push_swap.c
+++ srcs/push_swap/push_swap.c
@@ -6,27 +6,25 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/18 15:37:09 by sregnard          #+#    #+#             */
-/*   Updated: 2019/03/21 16:24:31 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/04/07 10:41:04 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int			main(int ac, char **av)
+int	main(int ac, char **av)
 {
-		t_ps	p;
+	t_ps p;
 
-		parse_args(&p, ac, av);
-		p.flags |= FLAG_SOLVER;
-		p.operations = NULL;
-		if (sorted(p, 0, p.size_a - 1, 0))
-						exit(EXIT_SUCCESS);
-		if (p.size_a <= 3)
-				mini_sort(&p, 'a');
-		else if (p.size_a <= 5)
-			selection_sort(&p);
-		else
-			quick_sort(&p, p.size_a - 1, 0);
-		ft_print_tab(opti_operations(&p.operations));
+	parse_args(&p, ac, av);
+	p.flags |= FLAG_SOLVER;
+	p.operations = NULL;
+	if (sorted(p, 0, p.size_a - 1, 0))
 		exit(EXIT_SUCCESS);
+	if (p.size_a <= 3)
+		mini_sort(&p, 'a');
+	else
+		selection_sort(&p);
+	ft_print_tab(opti_operations(&p.operations));
+	exit(EXIT_SUCCESS);
 }
--- srcs/push_swap/selection_sort.c
+++ srcs/push_swap/selection_sort.c
@@ -6,7 +6,7 @@
 /*   By: sregnard <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/14 11:25:34 by sregnard          #+#    #+#             */
-/*   Updated: 2019/03/21 13:58:54 by sregnard         ###   ########.fr       */
+/*   Updated: 2019/04/07 10:40:35 by sregnard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,49 @@
 
 static int	find_min(t_ps *p)
 {
-		int	i;
-		int	min;
-		int	pos;
+	int	i;
+	int	min;
+	int	pos;
 
-		if (p->size_a == 0)
-				return (-1);
-		i = p->size_a - 1;
-		min = p->a[i];
-		pos = i;
-		while (i--)
-				if (min > p->a[i])
-				{
-						min = p->a[i];
-						pos = i;
-				}
-		return (pos);
+	if (p->size_a == 0)
+		return (-1);
+	i = p->size_a - 1;
+	min = p->a[i];
+	pos = i;
+	while (i--)
+		if (min > p->a[i])
+		{
+			min = p->a[i];
+			pos = i;
+		}
+	return (pos);
 }
 
 static int	select_min(t_ps *p, int top)
 {
-		int	pos;
-		int	min;
-		int	(*f)(t_ps *, char);
+	int	pos;
+	int	min;
+	int	(*f)(t_ps *, char);
 
+	if (sorted(*p, 'a', p->size_a - 1, 0))
+		return (1);
+	pos = find_min(p);
+	min = p->a[pos];
+	f = (rotate_function(p, pos, 'a'));
+	while (p->a[p->size_a - 1] != min)
+	{
 		if (sorted(*p, 'a', p->size_a - 1, 0))
-				return (1);
-		pos = find_min(p);
-		min = p->a[pos];
-		f = (rotate_function(p, pos, 'a'));
-		while (p->a[p->size_a - 1] != min)
-		{
-				if (sorted(*p, 'a', p->size_a - 1, 0))
-						break ;
-				f(p, 'a');
-		}
-		push(p, 'b');
-		return (select_min(p, --top));
+			break ;
+		f(p, 'a');
+	}
+	push(p, 'b');
+	return (select_min(p, --top));
 }
 
 int			selection_sort(t_ps *p)
 {
-		select_min(p, p->size_a - 1);
-		while (p->size_b && push(p, 'a'))
-				;
-		return (1);
+	select_min(p, p->size_a - 1);
+	while (p->size_b && push(p, 'a'))
+		;
+	return (1);
 }
