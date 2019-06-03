9a9a3371402eb4016817301078f2760138d0db64
Stanley REGNARD <sregnard@e3r7p7.42.fr>
2019-06-03 15:39:57 +0200

    my_test

--- my_test.sh
+++ my_test.sh
@@ -6,14 +6,14 @@
 #    By: sregnard <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/20 02:23:04 by sregnard          #+#    #+#              #
-#    Updated: 2019/06/03 11:11:21 by sregnard         ###   ########.fr        #
+#    Updated: 2019/06/03 14:12:35 by sregnard         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 ARG=`ruby -e "puts((1..($1)).to_a.shuffle.join(' '))"`
 
-#./push_swap $ARG | ./checker $ARG -c -v
-#./push_swap $ARG | wc -l
-./push_swap $ARG
+./push_swap $ARG | ./checker $ARG -c -v
+./push_swap $ARG | wc -l
+#./push_swap $ARG
 #./checker $ARG
 echo $ARG
